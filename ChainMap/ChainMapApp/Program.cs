d1854b9b8182f92d033db4192e96a16f65571400
minislel <marcin4kt@gmail.com>
2024-05-06 15:44:44 +0200

    implemented chainmap, todo tests

---
+++ ChainMap/ChainMapApp/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using ChainMapLib;
+
+namespace ChainMapExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### Przykład użycia ChainMap ###\n");
+
+            // Tworzenie słowników
+            var dict1 = CreateDictionary("Pierwszy");
+            var dict2 = CreateDictionary("Drugi");
+            var dict3 = CreateDictionary("Trzeci");
+
+            // Tworzenie ChainMap
+            var chainMap = new ChainMap<string, string>(dict1, dict2, dict3);
+
+            while (true)
+            {
+                Console.WriteLine("\n### Menu ###");
+                Console.WriteLine("1. Wyświetl wszystkie wartości");
+                Console.WriteLine("2. Zmiana wartości dla klucza");
+                Console.WriteLine("3. Dodaj nowy słownik z wyborem priorytetu");
+                Console.WriteLine("4. Usuń klucz");
+                Console.WriteLine("5. Pobierz wszystkie klucze z ich źródłami");
+                Console.WriteLine("6. Pobierz wszystkie wartości z ich źródłami");
+                Console.WriteLine("7. Zakończ");
+
+                Console.Write("Wybierz operację: ");
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        ShowAllValues(chainMap);
+                        break;
+                    case "2":
+                        ChangeValueForKey(chainMap);
+                        break;
+                    case "3":
+                        AddNewDictionary(chainMap);
+                        break;
+                    case "4":
+                        RemoveKey(chainMap);
+                        break;
+                    case "5":
+                        GetAllKeysWithSources(chainMap);
+                        break;
+                    case "6":
+                        GetAllValuesWithSources(chainMap);
+                        break;
+                    case "7":
+                        return;
+                    default:
+                        Console.WriteLine("Nieprawidłowy wybór. Spróbuj ponownie.");
+                        break;
+                }
+            }
+        }
+
+        static void ShowAllValues(ChainMap<string, string> chainMap)
+        {
+            Console.WriteLine("\nWszystkie wartości:");
+            foreach (var kvp in chainMap)
+            {
+                Console.WriteLine($"Klucz: {kvp.Key}, Wartość: {kvp.Value}");
+            }
+        }
+
+        static void ChangeValueForKey(ChainMap<string, string> chainMap)
+        {
+            Console.WriteLine("\nDodawanie klucza:");
+
+            Console.Write("Podaj klucz do dodania: ");
+            var keyToAdd = Console.ReadLine();
+            Console.Write("Podaj wartość: ");
+            var value = Console.ReadLine();
+
+            try
+            {
+                chainMap[keyToAdd] = value;
+            }
+            catch
+            {
+                Console.WriteLine($"Klucz '{keyToAdd}' nie istnieje w ChainMap.");
+            }
+        }
+
+        static void AddNewDictionary(ChainMap<string, string> chainMap)
+        {
+            Console.WriteLine("\nDodawanie nowego słownika:");
+
+            var newDict = CreateDictionary("Nowy");
+
+            Console.Write("Podaj priorytet nowego słownika: ");
+            var priority = int.Parse(Console.ReadLine());
+
+            chainMap.AddDictionary(newDict, priority);
+            Console.WriteLine("Nowy słownik dodany do ChainMap.");
+        }
+
+        static void RemoveKey(ChainMap<string, string> chainMap)
+        {
+            Console.WriteLine("\nUsuwanie klucza:");
+
+            Console.Write("Podaj klucz do usunięcia: ");
+            var keyToRemove = Console.ReadLine();
+
+            if (chainMap.ContainsKey(keyToRemove))
+            {
+                if (chainMap.Remove(keyToRemove))
+                {
+                    Console.WriteLine($"Klucz '{keyToRemove}' usunięty.");
+                }
+                
+                else {
+                    Console.WriteLine($"Nie udało się usunąć klucza '{keyToRemove}', brakuje go w slowniku głównym");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Klucz '{keyToRemove}' nie istnieje w ChainMap.");
+            }
+        }
+
+
+        static void GetAllKeysWithSources(ChainMap<string, string> chainMap)
+        {
+            Console.WriteLine("\nPobieranie wszystkich kluczy z ich źródłami:");
+            var mainDictionary = chainMap.GetMainDictionary();
+            foreach (var v in mainDictionary)
+            {
+                Console.WriteLine($"Klucz: {v.Key}, Źródło: Słownik Główny");
+            }
+
+            for (int i = 0; i < chainMap.CountDictionaries(); i++)
+            {
+                var dictionary = chainMap.GetDictionary(i);
+                foreach (var key in dictionary.Keys)
+                {
+                    Console.WriteLine($"Klucz: {key}, Źródło: Słownik {i}");
+                }
+            }
+        }
+
+        static void GetAllValuesWithSources(ChainMap<string, string> chainMap)
+        {
+            Console.WriteLine("\nPobieranie wszystkich wartości z ich źródłami:");
+
+            var mainDictionary = chainMap.GetMainDictionary();
+            foreach (var v in mainDictionary)
+            {
+                Console.WriteLine($"Klucz: {v.Value}, Źródło: Słownik Główny");
+            }
+            for (int i = 0; i < chainMap.CountDictionaries(); i++)
+            {
+                var dictionary = chainMap.GetDictionary(i);
+                foreach (var kvp in dictionary)
+                {
+                    Console.WriteLine($"Klucz: {kvp.Key}, Wartość: {kvp.Value}, Źródło: Słownik {i}");
+                }
+            }
+        }
+        static Dictionary<string, string> CreateDictionary(string name)
+        {
+            var dictionary = new Dictionary<string, string>();
+
+            Console.WriteLine($"\nTworzenie słownika '{name}':");
+            while (true)
+            {
+                Console.Write("Podaj klucz (lub wpisz 'koniec' aby zakończyć): ");
+                var key = Console.ReadLine();
+                if (key.ToLower() == "koniec")
+                    break;
+
+                Console.Write("Podaj wartość: ");
+                var value = Console.ReadLine();
+
+                try
+                {
+                    dictionary.Add(key, value);
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Wartość dla podanego klucza już istnieje. Podaj inną wartość.");
+                }
+            }
+            return dictionary;
+        }
+    }
+}
---
+++ ChainMap/ChainMapLib/Class1.cs
@@ -0,0 +1,304 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace ChainMapLib
+{
+    public class ChainMap<TKey, TValue> : IDictionary<TKey, TValue>
+    {
+        private Dictionary<TKey, TValue> mainDictionary = new Dictionary<TKey, TValue>();
+        private List<Dictionary<TKey, TValue>> dictionaries = new List<Dictionary<TKey, TValue>>();
+        #region indexer this[]
+        public TValue this[TKey key]
+        {
+            get
+            {
+                if (mainDictionary.ContainsKey(key))
+                {
+                    return mainDictionary[key];
+                }
+                foreach (var d in dictionaries)
+                {
+                    if (d.ContainsKey(key))
+                    {
+                        return d[key];
+                    }
+                }
+                throw new KeyNotFoundException($"No value found for {key}");
+            }
+            set
+            {
+                bool found = false;
+                if (mainDictionary.ContainsKey(key))
+                {
+                    mainDictionary[key] = value;
+                    found = true;
+                }
+                foreach (var d in dictionaries)
+                {
+                    if (d.ContainsKey(key))
+                    {
+                        mainDictionary.Add(key, value);
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    throw new KeyNotFoundException($"No value found for {key}");
+                }
+
+            }
+        }
+        #endregion
+        #region IDictionary
+        public ICollection<TKey> Keys
+        {
+            get
+            {
+                var result = new List<TKey>();
+                foreach (var k in mainDictionary)
+                {
+                    result.Add(k.Key);
+                }
+                foreach (var d in dictionaries)
+                {
+                    foreach (var k in d)
+                    {
+                        result.Add(k.Key);
+                    }
+                }
+                return result;
+            }
+        }
+
+        public ICollection<TValue> Values
+        {
+            get
+            {
+                var result = new List<TValue>();
+                foreach (var k in mainDictionary)
+                { result.Add(k.Value); }
+                foreach (var d in dictionaries)
+                {
+                    foreach (var v in d)
+                    {
+                        result.Add(v.Value);
+                    }
+                }
+                return result;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                int result = 0;
+                foreach (var k in mainDictionary)
+                {
+                    result++;
+                }
+                foreach (var d in dictionaries)
+                {
+                    foreach (var v in d)
+                    {
+                        result++;
+                    }
+                }
+                return result;
+            }
+        }
+        
+
+        public ChainMap(params Dictionary<TKey, TValue>[] values)
+        {
+            foreach (var v in values)
+            {
+                dictionaries.Add(v);
+            }
+        }
+
+        public bool IsReadOnly => false;
+
+        public void Add(TKey key, TValue value)
+        {
+            if (mainDictionary.ContainsKey(key))
+            { throw new ArgumentException("the specified value already exists in the main (override) dictionary!"); }
+            mainDictionary.Add(key, value);
+        }
+
+        public bool TryAdd(TKey key, TValue value)
+        {
+            try
+            {
+                Add(key, value);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void Clear()
+        {
+            mainDictionary.Clear();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            if (mainDictionary.Contains(item))
+            { return true; }
+            foreach (var d in dictionaries)
+            {
+                if (d.Contains(item))
+                { return true; }
+            }
+            return false;
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            if (mainDictionary.ContainsKey(key))
+            { return true; }
+            foreach (var d in dictionaries)
+            {
+
+                if (d.ContainsKey(key))
+                { return true; }
+            }
+            return false;
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            List<Dictionary<TKey, TValue>> all = new List<Dictionary<TKey, TValue>>();
+            all.Add(mainDictionary);
+            foreach (var d in dictionaries)
+            {
+                all.Add(d);
+            }
+            foreach (var e in all)
+            {
+                foreach (var v in e)
+                {
+                    yield return new KeyValuePair<TKey, TValue>(v.Key, v.Value);
+                }
+            }
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (!mainDictionary.ContainsKey(key))
+            {
+                return false;
+            }
+            else
+            {
+                mainDictionary.Remove(key);
+                return true;
+            }
+        }
+
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+        {
+            if (mainDictionary.TryGetValue(key, out value) == true)
+            {
+                return true;
+            }
+            foreach (var d in dictionaries)
+            {
+                if (d.TryGetValue(key, out value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool ContainsValue(TValue value)
+        {
+            if (mainDictionary.ContainsValue(value))
+            { return true; }
+            foreach (var d in dictionaries)
+            {
+                if (d.ContainsValue(value))
+                { return true; }
+            }
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            mainDictionary[item.Key] = item.Value;
+        }
+        #endregion
+        #region ChainMap-Oriented methods
+        public void AddDictionary(Dictionary<TKey, TValue> dictionary, int priority)
+        {
+            dictionaries.Insert(priority, dictionary);
+        }
+        public void RemoveDictionary(int index)
+        { dictionaries.RemoveAt(index); }
+        public void ClearDictionaries()
+        {
+            dictionaries.Clear();
+            mainDictionary.Clear();
+        }
+        public int CountDictionaries() { return dictionaries.Count; }
+        public List<Dictionary<TKey, TValue>> GetDictionaries()
+        {
+            List<Dictionary<TKey, TValue>> result = new List<Dictionary<TKey, TValue>>();
+            result.Add(mainDictionary);
+            foreach (var d in dictionaries)
+            {
+                result.Add(d);
+            }
+            return result;
+        }
+        public Dictionary<TKey, TValue> GetDictionary(int index)
+        {
+return dictionaries[index]; 
+        }
+        public Dictionary<TKey, TValue> GetMainDictionary()
+        {
+            return mainDictionary;
+        }
+        public Dictionary<TKey, TValue> Merge()
+        {
+            var result = new Dictionary<TKey, TValue>();
+            foreach (var v in mainDictionary)
+            {
+                result.Add(v.Key, v.Value);
+            }
+            foreach (var d in dictionaries)
+            {
+                foreach (var v in d)
+                {
+                    if (!result.ContainsKey(v.Key))
+                    {
+                        result.Add(v.Key, v.Value);
+                    }
+                }
+            }
+            return result;
+        }
+        #endregion
+    }
+}
